07320ead0a9cf3837adb01a102e1dffbf48e87a0
FlyingSheep22 <kevinbai222@gmail.com>
2024-11-23 12:40:13 -0500

    temp: task list

--- Assets/Task.cs
+++ Assets/Task.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Task : MonoBehaviour
 {
@@ -18,5 +19,18 @@ public class Task : MonoBehaviour
         0,0.4f);
         yield return new WaitForSecondsRealtime(0.4f);
         Destroy(gameObject);
+    }
+
+    public void StopTyping(){
+        TMP_InputField input = GetComponentInChildren<TMP_InputField>();
+        
+        if (string.IsNullOrWhiteSpace(input.text))
+        {
+            Debug.Log("Input field is empty. Not disabling the field.");
+            return; // Exit if no text was entered
+        }
+
+        input.interactable = false;
+        input.GetComponent<Image>().enabled = false;    
     }
 }
